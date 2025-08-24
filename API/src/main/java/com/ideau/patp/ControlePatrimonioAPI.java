cf0b404ae0e0d564d515dc43beda5ca912fd9a77
eduardogm22 <167825825+eduardogm22@users.noreply.github.com>
2025-08-24 16:42:04 -0300

    Configurado para ler usuário e senha do arquivo .env
    Criar arquivo .env na raiz do repo, com
    DB_USERNAME=
    DB_PASSWORD=

--- API/src/main/java/com/ideau/patp/ControlePatrimonioAPI.java
+++ API/src/main/java/com/ideau/patp/ControlePatrimonioAPI.java
@@ -1,5 +1,7 @@
 package com.ideau.patp;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ControlePatrimonioAPI {
 
 	public static void main(String[] args) {
+        Dotenv dotenv = new DotenvBuilder().load();
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
 		SpringApplication.run(ControlePatrimonioAPI.class, args);
 	}
-
 }
