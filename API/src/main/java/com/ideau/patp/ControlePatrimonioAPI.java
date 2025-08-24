7f5462f1c70b7f5bf238d746ce5126b3e2ef9f29
eduardogm22 <167825825+eduardogm22@users.noreply.github.com>
2025-08-24 16:11:54 -0300

    Reorganizado projeto.

---
+++ API/src/main/java/com/ideau/patp/ControlePatrimonioAPI.java
@@ -0,0 +1,13 @@
+package com.ideau.patp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlePatrimonioAPI {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlePatrimonioAPI.class, args);
+	}
+
+}
--- target/classes/README.md
+++
@@ -1,2 +0,0 @@
-# PATP252-GDP
-Projeto de Aperfeiçoamento Teórico Prático - Gestão de Patrimônios ( Análise e Desenvolvimento de Sistemas 4º Semestre )
