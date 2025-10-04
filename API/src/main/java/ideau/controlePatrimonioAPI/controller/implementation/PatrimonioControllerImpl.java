fa4bac08987d17016dc22bcab51f1d7d3bc5573a
eduardogm22 <167825825+eduardogm22@users.noreply.github.com>
2025-10-04 00:01:11 -0300

    CRUD Patrimonios

--- API/src/main/java/ideau/controlePatrimonioAPI/controller/implementation/PatrimonioControllerImpl.java
+++ API/src/main/java/ideau/controlePatrimonioAPI/controller/implementation/PatrimonioControllerImpl.java
@@ -3,12 +3,21 @@ package ideau.controlePatrimonioAPI.controller.implementation;
 import ideau.controlePatrimonioAPI.controller.PatrimonioController;
 import ideau.controlePatrimonioAPI.model.Patrimonio;
 import ideau.controlePatrimonioAPI.model.PatrimonioDTO;
+import ideau.controlePatrimonioAPI.services.implementation.PatrimonioServiceImpl;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Map;
 
+@RestController
 public class PatrimonioControllerImpl implements PatrimonioController {
+    private final PatrimonioServiceImpl svc;
+
+    public PatrimonioControllerImpl(PatrimonioServiceImpl svc) {
+        this.svc = svc;
+    }
+
     @Override
     public List<PatrimonioDTO> retornaPorIdStatus(Long idStatus) {
         return List.of();
@@ -26,7 +35,7 @@ public class PatrimonioControllerImpl implements PatrimonioController {
 
     @Override
     public Map<Integer, PatrimonioDTO> cadastra(Map<Integer, Patrimonio> mapObjs) {
-        return Map.of();
+        return svc.cadastrarLote(mapObjs);
     }
 
     @Override
